55b205a381eca99e41e746fbe1568634b2fb8078
Erreur32 <erreur32@echosystem.fr>
2026-04-09 18:36:07 +0200

    feat: Version 0.7.63 - Mise à jour

--- server/plugins/unifi/UniFiPlugin.ts
+++ server/plugins/unifi/UniFiPlugin.ts
@@ -195,7 +195,7 @@ export class UniFiPlugin extends BasePlugin {
     private readonly BANDWIDTH_MAX = 20160; // 7 days at 30s polling
 
     constructor() {
-        super('unifi', 'UniFi Controller', '0.7.62');
+        super('unifi', 'UniFi Controller', '0.7.63');
         this.apiService = new UniFiApiService();
     }
 
--- src/constants/version.ts
+++ src/constants/version.ts
@@ -4,7 +4,7 @@
  * Do not modify this file manually - use the update script instead
  */
 
-export const APP_VERSION = '0.7.62';
+export const APP_VERSION = '0.7.63';
 
 /**
  * Environment information from server
